3c39e6b00ac49f8a3aa8e83734d24f1df245e677
Navoznov, Ivan E <Navoznov.IE@parma.ru>
2020-12-02 11:39:59 +0300

    day02 - part 2

--- Day02/Program.py
+++ Day02/Program.py
@@ -5,10 +5,7 @@ def parseLine(line):
     match = re.match(pattern, line)
     return match.groups(0)
 
-def isValidPasswordObj(obj):
-    return isValidPassword(*obj)
-
-def isValidPassword(minCount, maxCount, letter, password):
+def isValidPasswordPart1(minCount, maxCount, letter, password):
     minCount = int(minCount)
     maxCount = int(maxCount)
     counter = 0
@@ -20,16 +17,27 @@ def isValidPassword(minCount, maxCount, letter, password):
 
     return True if counter >= minCount else False
 
-minCount, maxCount, letter, password = parseLine("16-20 h: hhhjthhhtphchpkhmhhh")
-assert(isValidPasswordObj(parseLine("2-4 p: vpkpp")) == True)
-assert(isValidPasswordObj(parseLine("2-4 v: vpkpp")) == False)
+def isValidPasswordPart2(position1, position2, letter, password):
+    letter1 = password[int(position1)-1]
+    letter2 = password[int(position2)-1]
+    return letter1 == letter and letter2 != letter or letter1 != letter and letter2 == letter
 
 lines = open('Day02\input.txt').read().split('\n')
 
+# part 1
 validPasswordsCounter = 0
 for line in lines:
     minCount, maxCount, letter, password = parseLine(line)
-    if isValidPassword(minCount, maxCount, letter, password):
+    if isValidPasswordPart1(minCount, maxCount, letter, password):
+        validPasswordsCounter += 1
+
+print(validPasswordsCounter)
+
+# part 2
+validPasswordsCounter = 0
+for line in lines:
+    position1, position2, letter, password = parseLine(line)
+    if isValidPasswordPart2(position1, position2, letter, password):
         validPasswordsCounter += 1
 
 print(validPasswordsCounter)
