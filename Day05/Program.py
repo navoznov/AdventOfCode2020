467f69a61b978bd3ff2386b3bb985f6736b13c17
Navoznov, Ivan E <Navoznov.IE@parma.ru>
2020-12-07 18:00:18 +0300

    Day05 - part 2

--- Day05/Program.py
+++ Day05/Program.py
@@ -8,8 +8,19 @@ def parseLine(line):
     column = int(''.join([replacements[x] for x in line[-3:]]), 2)
     return (row, column)
 
+
 def getSeatId(row, column):
     return row * 8 + column
 
+
 seatIds = [getSeatId(*(parseLine(x))) for x in lines]
+
+# part 1
 print(max(seatIds))
+
+# part 2
+seatIds.sort()
+for i in range(len(seatIds)):
+    if i+seatIds[0] != seatIds[i]:
+        print(i+seatIds[0])
+        break
