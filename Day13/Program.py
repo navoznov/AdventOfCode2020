cfde198ee3d759b21f6b19212d2bebb5023ca0d1
Navoznov, Ivan E <Navoznov.IE@parma.ru>
2020-12-15 16:17:51 +0300

    Day13 - part 2

--- Day13/Program.py
+++ Day13/Program.py
@@ -17,61 +17,24 @@ for i in range(len(buses)):
     if bus == 'x':
         continue
 
-    bus_times.append((int(bus), i))
+    bus_times.append((int(bus), -i % int(bus)))
 
 bus_times.sort()
 bus_times.reverse()
 
-def check_timestamp(timestamp, bus_times):
-    for bus, time in bus_times:
-        if (timestamp + time) % bus != 0:
-            return False
+i = 0
+bus_info = bus_times[0]
+bus, offset = bus_info
+timestamp = bus + offset
+timestamp_increment = bus
 
-    return True
+for i in range(1, len(bus_times)):
+    bus_info = bus_times[i]
+    bus, offset = bus_info
 
+    while timestamp % bus != offset:
+        timestamp += timestamp_increment
 
-#[(787, 48), (523, 17), (41, 7), (37, 54), (29, 77), (23, 40), (19, 36), (17, 0), (13, 35)]
+    timestamp_increment *= bus
 
-# 787 48
-# 523 17
-# 41  7
-# 37  54
-# 29  77
-# 23  40
-# 19  36
-# 17  0
-# 13  35
-
-def get_timestamp():
-    increment, time = bus_times[0]
-    start = int("203,480,886,435,643".replace("," ,""))
-    timestamp = (start  - (start % increment)) - time
-    timestamp -= increment
-    while True:
-        timestamp += increment
-        if (timestamp) % 17 != 0:
-            continue
-        if (timestamp + 17) % 523 != 0:
-            continue
-        if (timestamp + 7) % 41 != 0:
-            continue
-        if (timestamp + 54) % 37 != 0:
-            continue
-        if (timestamp + 77) % 29 != 0:
-            print(f"{timestamp:,}")
-            continue
-        if (timestamp + 40) % 23 != 0:
-            continue
-        if (timestamp + 36) % 19 != 0:
-            continue
-        if (timestamp + 35) % 13 != 0:
-            continue
-
-        return timestamp
-        # if check_timestamp(timestamp, bus_times):
-        #     return timestamp
-
-timestamp = get_timestamp()
-print("!!!   !!!   !!!")
-print(timestamp) # > 133151790670903
-
+print(timestamp)
